2c7b46fc02e239b64e94209dd68498f82284ba93
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-01-19 12:26:46 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,8 @@
+#CS175L-01
+#Sophia Ramirez Velandia
+#myInfo
+
+print("Sophia Ramirez Velandia")
+print("431 Cedar Ave, Mullaney Hall Room 241, West Long Branch, NJ 07764")
+print("+1 3474086001")
+

f021eaeb086e232e1dd8b7c079a94a1e3907a88f
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-01-23 16:17:39 -0500

    Stocks

---
+++ stocks.py
@@ -0,0 +1,26 @@
+#CS175L
+#Sophia Ramirez Velandia
+#Stocks
+
+num_shares= 2000
+purchase_price= 40.0
+selling_price= 42.75
+commission_rate= 0.03
+
+amountPaidForStock = num_shares * purchase_price
+purchaseCommission = commission_rate * amountPaidForStock
+totalPaid = amountPaidForStock + purchaseCommission
+stockSoldFor = num_shares * selling_price
+sellingCommission = commission_rate * stockSoldFor
+totalReceived = stockSoldFor - sellingCommission
+profitOrLoss = totalReceived - totalPaid
+
+print('Amount paid for the stock: $',amountPaidForStock)
+
+print('Commission paid on the purchase: $',purchaseCommission)
+
+print('Amount the stock sold for: $',stockSoldFor)
+
+print('Commission paid on the sale: $',sellingCommission)
+
+print('Profit (or loss if negative): $',profitOrLoss)

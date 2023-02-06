08d9de0d2c55b77e946e49875af9394f0ace7300
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-02-06 12:54:27 -0500

    restaurant selector v2 while loop

--- restaurant selector.py
+++ restaurant selector.py
@@ -1,40 +1,46 @@
 #CS175L
 #Sophia Ramirez Velandia
-#restaurant
+#restaurant V2
 #Professor Gil
 
 vegetarian= False
 vegan= False
 gluten_free= False
 
-answer1= input('Is anyone in your party a vegetarian? ')
-answer1lower=answer1.lower()
-if answer1 == 'yes':
-    vegetarian = True  
+keep_going= 'yes'
+while keep_going =='yes':
+    answer1= input('Is anyone in your party a vegetarian? ')
+    answer1lower=answer1.lower()
+    if answer1 == 'yes':
+        vegetarian = True  
 
-answer2= input('Is anyone in your party a vegan? ')
-answer2lower=answer2.lower()
-if answer2 == 'yes':
-    vegan = True
+    answer2= input('Is anyone in your party a vegan? ')
+    answer2lower=answer2.lower()
+    if answer2 == 'yes':
+        vegan = True
 
-answer3= input('Is anyone in your party gluten-free? ')
-answer3lower=answer3.lower()
-if answer3 == 'yes':
-    gluten_free = True
+    answer3= input('Is anyone in your party gluten-free? ')
+    answer3lower=answer3.lower()
+    if answer3 == 'yes':
+        gluten_free = True
 
-print('\nHere are your restaurant choices:')
+    print('\nHere are your restaurant choices:')
     
-if not vegetarian and not vegan and gluten_free:
-    print("Joe's Gourmet Burgers")
+    if not vegetarian and not vegan and gluten_free:
+        print("Joe's Gourmet Burgers")
 
-elif not vegan and not gluten_free:
-    print("Mama's Fine Italian")
+    elif not vegan and not gluten_free:
+        print("Mama's Fine Italian")
 
-elif not vegan:
-    print("Main Street Pizza")
+    elif not vegan:
+        print("Main Street Pizza")
 
-print("Corner Cafe")
-print("Chef's Kitchen")
+    print("Corner Cafe")
+    print("Chef's Kitchen")
+
+
+    keep_going= input('Do you want to do keep going? ')
+
     
 
     

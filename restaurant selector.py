7ecc31529c499793ebe092141cdde402d94a1a89
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-02-13 13:33:25 -0500

    restaurant selector v1

---
+++ restaurant selector.py
@@ -0,0 +1,48 @@
+#CS175L
+#Sophia Ramirez Velandia
+#restaurant v2
+#Professor Gil
+
+vegetarian= False
+vegan= False
+gluten_free= False
+
+answer1= input('Is anyone in your party a vegetarian? ')
+answer1lower=answer1.lower()
+if answer1 == 'yes':
+    vegetarian = True  
+
+answer2= input('Is anyone in your party a vegan? ')
+answer2lower=answer2.lower()
+if answer2 == 'yes':
+    vegan = True
+
+answer3= input('Is anyone in your party gluten-free? ')
+answer3lower=answer3.lower()
+if answer3 == 'yes':
+    gluten_free = True
+
+print('\nHere are your restaurant choices:')
+    
+if not vegetarian and not vegan and not gluten_free:
+    print("Joe's Gourmet Burgers")
+
+if not vegan and not gluten_free:
+    print("Mama's Fine Italian")
+
+if not vegan:
+    print("Main Street Pizza")
+
+print("Corner Cafe")
+print("Chef's Kitchen")
+
+
+    
+
+
+
+    
+
+    
+
+

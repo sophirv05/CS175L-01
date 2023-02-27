bec987f3322bec33211de103d9081114043909bd
sophirv05 <114093203+sophirv05@users.noreply.github.com>
2023-02-27 14:07:26 -0500

    Average and grade assignment V2

--- Average and Grade Assignment.py
+++ Average and Grade Assignment.py
@@ -1,50 +1,60 @@
 #CS 175L
 #Prof Gil
 #Sophia Ramirez Velandia
-#Chapter 5 - Calculate Average and Grade with Functions
+#Chapter 5 - Calculate Average and Grade with Functions V2
 
-def calc_average():
-    grade1=float(input('Enter score 1: '))
-    grade2=float(input('Enter score 2: '))
-    grade3=float(input('Enter score 3: '))
-    grade4=float(input('Enter score 4: '))
-    grade5=float(input('Enter score 5: '))
-    total=grade1+grade2+grade3+grade4+grade5
-    test=5
-    avg = total/test
-    return grade1, grade2, grade3, grade4, grade5,avg
+import myrandom as r
 
-def determine_grade(numeric_grade):  
-    if numeric_grade>=90 and numeric_grade<=100:
-        return 'A'
-    elif numeric_grade>=80 and numeric_grade<90:
-        return 'B' 
-    elif numeric_grade>=70 and numeric_grade<80:
-        return 'C'
-    elif numeric_grade>=60 and numeric_grade<70:
-        return 'D'
-    elif numeric_grade<60 and numeric_grade>=0:
-        return 'F'
+def calc_average(score):
+    total=0
+    for x in range(5):
+        total+=score[x]
+    avg = total/5
+    return avg
 
+def determine_grade(score):
+        if score>=90 and score<=100:
+            gra= 'A'
+        elif score>=80 and score<90:
+            gra= 'B' 
+        elif score>=70 and score<80:
+            gra= 'C'
+        elif score>=60 and score<70:
+            gra= 'D'
+        elif score<60 and score>=0:
+            gra= 'F'
+        return gra
+
+'''
+def my_random(num1,num2):
+    score=r.randint(num1,num2)
+    return score
+'''
+
+def enter_scores(score):
+    for x in range(5):
+        s=int(input(f'Enter score [x+1]: '))
+        score.append(s)
+    return score
 
 def main():
     question='yes'
+    score=[]
     while question=='yes':
-        grade1, grade2, grade3, grade4, grade5, avg=calc_average()
-        print()
+        for x in range(5):
+            grade=r.my_random(1,100)
+            score.append(grade)
         print('Score', f'{"Numeric Grade   Letter Grade":>35}')
         print('---------------------------------------------------')
-        print(f'score 1:  {grade1:>15}',f'{determine_grade(grade1):>7}')
-        print(f'score 2:  {grade2:>15}',f'{determine_grade(grade2):>7}')
-        print(f'score 3:  {grade3:>15}',f'{determine_grade(grade3):>7}')
-        print(f'score 4:  {grade4:>15}',f'{determine_grade(grade4):>7}')
-        print(f'score 5:  {grade5:>15}',f'{determine_grade(grade5):>7}')
-    
+        for y in range(5):
+            print(f'score {y}:  {score[y]:>15}',f'{determine_grade(score[y]):>7}')
+        
+        avg=calc_average(score)
         print(f'Average score: {avg:>10}', f'{determine_grade(avg):>7}')
         print()
 
-
         question=input('Enter "yes" if you would like to do another calculation: ')
+    print()
 
 main()
 
---
+++ myrandom.py
@@ -0,0 +1,4 @@
+import random as r
+def my_random(num1,num2):
+    score=r.randint(num1,num2)
+    return score
